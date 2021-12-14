56ffce2df6f1263c83da0cb658b9131b9fea9c8c
bernardofnoro <bernardofn@outlook.com>
2021-12-13 21:40:14 -0300

    chore: swagger update

--- netflix-remake/src/main.ts
+++ netflix-remake/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  const config = new DocumentBuilder()
+    .setTitle('postgres + Prisma + Typescript')
+    .setDescription('API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apiswagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
